74fd71e18765bd431df26d966a3ae40ec048ca71
Dmitry Surchin <dsurchin@gmail.com>
2024-10-16 20:48:41 +0200

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <H1>The Wild Oasis</H1>
+      <Button>Check in</Button>
+    </div>
+  );
 }
 
 export default App;
